3558c72d9d28cf7875001649d6ce0ce469387a39
Hamta Rahmani <152522543+hamtarahmani@users.noreply.github.com>
2024-01-21 12:02:35 +0330

    add solve function

--- main.cpp
+++ main.cpp
@@ -10,6 +10,233 @@ using namespace std;
 int tool;
 bool check= false;
 
+typedef enum
+{
+    BLACK = 0, BLUE = 1, GREEN = 2,
+    AQUA = 3, RED = 4, PURPLE = 5,
+    YELLOW = 6, WHITE = 7, GRAY = 8,
+    LIGHT_BLUE = 9, LIGHT_GREEN = 10,
+    LIGHT_AQUA = 11, LIGHT_RED = 12,
+    LIGHT_PURPLE = 13, LIGHT_YELLOW = 14,
+    LIGHT_WHITE = 15
+} ConsoleColors;
+typedef HANDLE Handle;
+typedef CONSOLE_SCREEN_BUFFER_INFO BufferInfo;
+typedef WORD Word;
+short setTextColor(const ConsoleColors foreground)
+{
+    Handle consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+    BufferInfo bufferInfo;
+    if(!GetConsoleScreenBufferInfo(consoleHandle, &bufferInfo))
+        return 0;
+    Word color = (bufferInfo.wAttributes & 0xF0) + (foreground & 0x0F);
+    SetConsoleTextAttribute(consoleHandle, color);
+    return 1;
+}
+
+
+
+void GPS (int x, int y, int target_x , int target_y, int**& copy_maze , int sum , int**& number) {
+
+
+
+    if ( sum-copy_maze[x][y] == 0 && (( x + 1 == target_x && y == target_y ) ||( x == target_x && y + 1 == target_y))) {
+        check = true;
+        copy_maze[x][y]=0;
+        return;
+    }
+    // Use the <random> header to generate random numbers
+    random_device rd;
+    mt19937 gen(rd());
+    shuffle(order.begin(), order.end(), gen);
+
+    for (int i = 0; i < 4; ++i) {
+
+
+        if (copy_maze[x + 1][y] != 0 && order[i] == 1) {
+            copy_maze[x][y] = 0;
+            GPS(x + 1, y, target_x, target_y, copy_maze, sum - number[x][y], number);
+            if (check) {
+                return;
+            } else {
+                copy_maze[x][y] = number[x][y];
+            }
+        }
+
+         if (copy_maze[x - 1][y] != 0 && order[i] == 2) {
+            copy_maze[x][y] = 0;
+            GPS(x - 1, y, target_x, target_y, copy_maze, sum - number[x][y], number);
+            if (check) {
+                return;
+            } else {
+                copy_maze[x][y] = number[x][y];
+            }
+        }
+
+         if (copy_maze[x][y + 1] != 0 &&order[i] == 3) {
+            copy_maze[x][y] = 0;
+            GPS(x, y + 1, target_x, target_y, copy_maze, sum - number[x][y], number);
+            if (check) {
+                return;
+            } else {
+                copy_maze[x][y] = number[x][y];
+            }
+        }
+
+         if (copy_maze[x][y - 1] != 0 && order[i] == 4) {
+            copy_maze[x][y] = 0;
+            GPS(x, y - 1, target_x, target_y, copy_maze, sum - number[x][y], number);
+            if (check) {
+                return;
+            } else {
+                copy_maze[x][y] = number[x][y];
+            }
+        }
+    }
+
+    copy_maze[x][y]=number[x][y];
+
+}
+
+void Solve(int command){
+    string names,choose;
+    fstream maps("Maps/names.txt",ios::in);
+    int m=0;
+
+
+
+
+    setTextColor(LIGHT_BLUE);
+
+    while (getline(maps,names)){
+        cout<<m+1<<"."<<names<<endl;
+        m++;
+    }
+
+
+    setTextColor(LIGHT_WHITE);
+    cout<<"which one !?";cin>>choose;
+
+
+
+    maps.close();
+
+    fstream map ("Maps/"+choose+".txt",ios::in);
+    string generate,line="";
+    while (getline(map,generate)){
+        line+=generate;
+    }
+
+    int x=0,y=0;
+    for (int i = 0; i < line.length(); ++i) {
+        if (line[i]=='#'){
+            y++;
+        }
+        else if (line[i]=='!'){
+            x++;
+        }
+    }
+
+    y=y/x;
+
+    string playgroud[x][y];
+
+    int k=-1,n=-1;
+
+    for (int i = 0; i < line.length(); ++i) {
+         if (line[i]=='#'){
+            k++;
+        }
+        else if (line[i]=='!'){
+            n++;
+            k=-1;
+        }
+         else{
+             playgroud[n][k]+=line[i];
+         }
+    }
+
+
+
+
+    int **copy_map;
+    copy_map=new int *[x+2];
+    for (int i = 0; i < x+2; ++i) {
+        copy_map[i]=new int [y+2];
+        for (int j = 0; j < y+2 ; ++j) {
+
+            copy_map[i][j]=0;
+
+        }
+    }
+
+    for (int i = 0; i < x; ++i) {
+        for (int j = 0; j < y; ++j) {
+            copy_map[i+1][j+1]= stoi(playgroud[i][j]);
+        }
+    }
+    copy_map[x][y]=0;
+
+
+
+    int ** number;
+    number = new int *[x+2];
+
+    for (int i = 0; i < x+2; ++i) {
+        number[i]=new int [y+2];
+        for (int j = 0; j < y+2 ; ++j) {
+
+            number[i][j]=0;
+
+        }
+    }
+
+    for (int i = 0; i < x; ++i) {
+        for (int j = 0; j < y; ++j) {
+            number[i+1][j+1]= stoi(playgroud[i][j]);
+        }
+    }
+    number[x][y]=0;
+
+
+
+
+    GPS(1,1,x,y,copy_map, stoi(playgroud[x-1][y-1]),number);
+
+    check= false;
+
+    cout<<endl;
+
+    for (int i = 0; i <x ; ++i) {
+        for (int j = 0; j <y ; ++j) {
+            cout << '|';
+            if (copy_map[i+1][j+1] == 0 && stoi(playgroud[i][j]) != 0 && (i != x-1 || j != y-1)) {
+                setTextColor(LIGHT_AQUA);
+                cout << setw(2) << setfill(' ')<<stoi(playgroud[i][j])<<setw(2)<<setfill(' ') ;
+                setTextColor(LIGHT_WHITE);
+            }
+            else if(copy_map[i+1][j+1] == 0 && stoi(playgroud[i][j]) == 0){
+                setTextColor(LIGHT_YELLOW);
+                cout << setw(2) << setfill(' ')<<string (1,2)<<setw(2)<<setfill(' ') ;
+                setTextColor(LIGHT_WHITE);
+
+            }
+            else if (i == x-1 && j == y-1){
+                setTextColor(LIGHT_AQUA);
+                cout << setw(2) << setfill(' ')<<stoi(playgroud[i][j])<<setw(2)<<setfill(' ') ;
+                setTextColor(LIGHT_WHITE);
+            }
+            else{
+                setTextColor(GRAY);
+                cout << setw(2) << setfill(' ')<<stoi(playgroud[i][j])<<setw(2)<<setfill(' ') ;
+                setTextColor(LIGHT_WHITE);
+            }
+        }
+        cout<<setw(2)<<setfill(' ')<<"|"<<endl;
+    }
+
+}
+
 vector<int> order = {1, 2, 3, 4};
 
 void path (int x, int y, int length, int**& copy_maze) {
@@ -202,7 +429,7 @@ void EasyMap(int command){
 
 
     for (int i = 0 ; i < x ; ++i) {
-
+        map<<"!";
         for (int j = 0; j < y ; ++j) {
 
             if (playground[i][j]==9 && !(i==x-1&&j==y-1)){
@@ -221,7 +448,7 @@ void EasyMap(int command){
                 }
 
             }
-            map<<playground[i][j]<<" ";
+            map<<"#"<<playground[i][j];
         }
         map<<endl;
     }
@@ -318,6 +545,7 @@ void HardMap(int command){
 
 
     for (int i = 0; i < x ; ++i) {
+        map<<"!";
         for (int j = 0; j < y; ++j) {
             if (playground[i][j]=="#"){
                 random_number=rand()%(au-al+1)+al;
@@ -326,7 +554,7 @@ void HardMap(int command){
                 }
                 playground[i][j]= to_string(random_number);
             }
-            map<<playground[i][j]<<" ";
+            map<<"#"<playground[i][j];
         }
         map<<endl;
     }
